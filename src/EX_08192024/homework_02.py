7d6dc37ebce2c9dff8681c695fb29101d159dbb9
Priyanka <priyaanka94@gmail.com>
2024-08-26 17:12:22 +0530

    added home work

--- src/EX_08192024/homework_02.py
+++ src/EX_08192024/homework_02.py
@@ -4,3 +4,15 @@
 #isosceles (exactly two sides are equal), or scalene (no sides are equal).
 #Use an if-else statement to classify the triangle.
 
+
+print("enter three sides of the triangle as a,b,c")
+a=int(input("enter a :"))
+b=int(input("enter b :"))
+c=int(input("enter c :"))
+
+if a==b and b==c :
+    print("Triangle is an Equilateral triangle")
+elif a == b or c == a or b == c:
+    print("Triangle is an isosceles triangle")
+else:
+    print("Triangle is an scalene triangle")
