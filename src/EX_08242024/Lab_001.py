942c806eecc4c72ffab482db999acb89cbd4c2ff
Priyanka <priyaanka94@gmail.com>
2024-08-31 20:52:02 +0530

    lambda

---
+++ src/EX_08242024/Lab_001.py
@@ -0,0 +1,17 @@
+
+def print_lis():
+    print("hello world")
+
+def print_list(a, b, c):
+    return a+b+c
+
+
+print_lis()
+
+
+ab=input("enter a char")
+ac =input("enter a char2")
+ad=input("enter a char3")
+
+sd = print_list(ab,ac,ad)
+print(sd)
---
+++ src/EX_08242024/function with return.py
@@ -0,0 +1,13 @@
+def maxi(*args):
+    return max(*args)
+
+a= maxi(12,45,16,346)
+print(a)
+
+b=90
+
+d = lambda a,b: a+b
+print(d(12,23))
+
+e= lambda q,w,e : q+w+e
+print(e("i " ,"love " ,"India" ))
